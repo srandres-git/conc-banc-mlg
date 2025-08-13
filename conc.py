995ccb3852a40d60084a9c17cd93391c7399962a
srandres-git <andysanchez2001@gmail.com>
2025-08-13 14:07:14 -0600

    Fix: comment out erroneous aggregation logic for "COMISIÓN" and "IVA DE COMISIÓN" movements in format_edo_cta function

--- conc.py
+++ conc.py
@@ -168,35 +168,38 @@ def format_edo_cta(edo_cta_cves: pd.DataFrame, periodo: tuple[date,date]) -> pd.
         edo_cta_cves['CARGO'],
         edo_cta_cves['ABONO']
     )
+    
+    # TODO: este código elimina erróneamente otros movinimientos que no son comisión ni IVA de comisión
+    # queda pendiente revisar
 
-    # agrupamos los movimientos que tengan tipo de movimiento "COMISIÓN" e "IVA DE COMISIÓN"
-    # que estén en la misma cuenta y con la misma fecha
-    # separamos las columnas de edo_cta por tipo y función de agregación
-    agg_edo_cta = {
-        col: 'sum' for col in edo_cta_cves.columns if col in ['CARGO', 'ABONO', 'IMPORTE']
-    }
-    agg_edo_cta['FECHA'] = 'first'  # preservamos la primera fecha, puesto que es la misma para todos los movimientos de la misma clave
-    # saldo tomamos el último saldo del periodo
-    agg_edo_cta['SALDO'] = 'last'
-    # el resto de columnas las concatenamos
-    agg_edo_cta.update({
-        col: lambda x: ', '.join([y if not y is None else '' for y in x.unique() ]) for col in edo_cta_cves.columns if col not in agg_edo_cta
-    })
+    # # agrupamos los movimientos que tengan tipo de movimiento "COMISIÓN" e "IVA DE COMISIÓN"
+    # # que estén en la misma cuenta y con la misma fecha
+    # # separamos las columnas de edo_cta por tipo y función de agregación
+    # agg_edo_cta = {
+    #     col: 'sum' for col in edo_cta_cves.columns if col in ['CARGO', 'ABONO', 'IMPORTE']
+    # }
+    # agg_edo_cta['FECHA'] = 'first'  # preservamos la primera fecha, puesto que es la misma para todos los movimientos de la misma clave
+    # # saldo tomamos el último saldo del periodo
+    # agg_edo_cta['SALDO'] = 'last'
+    # # el resto de columnas las concatenamos
+    # agg_edo_cta.update({
+    #     col: lambda x: ', '.join([y if not y is None else '' for y in x.unique() ]) for col in edo_cta_cves.columns if col not in agg_edo_cta
+    # })
 
-    # agrupamos por las columnas clave y aplicamos las funciones de agregación
-    edo_cta_grouped = edo_cta_cves[edo_cta_cves['TIPO MOVIMIENTO'].isin(['COMISIÓN', 'IVA DE COMISIÓN'])].groupby(
-        ['BANCO', 'CUENTA', 'FECHA',],
-        as_index=False
-    ).agg(agg_edo_cta)
-    # extraemos las claves de movimiento bancario que tienen más de un movimiento
-    duplicated_keys = edo_cta_grouped['CLAVE'].str.split(',').explode().unique()
-    # quitamos del original los que tienen clave de movimiento bancario duplicada y sean "COMISIÓN" o "IVA DE COMISIÓN"
-    # en su lugar concatenamos los de edo_cta_grouped
-    edo_cta_cves.drop(edo_cta_cves[
-        edo_cta_cves['CLAVE'].isin(duplicated_keys) &
-        edo_cta_cves['TIPO MOVIMIENTO'].isin(['COMISIÓN', 'IVA DE COMISIÓN'])
-    ].index, inplace=True)
-    edo_cta_cves = pd.concat([edo_cta_cves, edo_cta_grouped], ignore_index=True)
+    # # agrupamos por las columnas clave y aplicamos las funciones de agregación
+    # edo_cta_grouped = edo_cta_cves[edo_cta_cves['TIPO MOVIMIENTO'].isin(['COMISIÓN', 'IVA DE COMISIÓN'])].groupby(
+    #     ['BANCO', 'CUENTA', 'FECHA',],
+    #     as_index=False
+    # ).agg(agg_edo_cta)
+    # # extraemos las claves de movimiento bancario que tienen más de un movimiento
+    # duplicated_keys = edo_cta_grouped['CLAVE'].str.split(',').explode().unique()
+    # # quitamos del original los que tienen clave de movimiento bancario duplicada y sean "COMISIÓN" o "IVA DE COMISIÓN"
+    # # en su lugar concatenamos los de edo_cta_grouped
+    # edo_cta_cves.drop(edo_cta_cves[
+    #     edo_cta_cves['CLAVE'].isin(duplicated_keys) &
+    #     edo_cta_cves['TIPO MOVIMIENTO'].isin(['COMISIÓN', 'IVA DE COMISIÓN'])
+    # ].index, inplace=True)
+    # edo_cta_cves = pd.concat([edo_cta_cves, edo_cta_grouped], ignore_index=True)
 
     return edo_cta_cves
 
